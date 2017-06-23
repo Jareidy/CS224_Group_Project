e7147df9913cc1c5ac8e87151165012b0a42e095
Jordan Reidy <jajreidy@gmail.com>
2017-06-23 14:14:42 -0400

    Expanded Data Parser for testing. Working on adding test cases for each class.

--- test/Tests.java
+++ test/Tests.java
@@ -1,14 +1,19 @@
 import javafx.scene.image.Image;
-import model.*;
+import model.ImageManager;
+import model.ImportFile;
+import model.Picture;
+import model.PictureDataParser;
 import org.junit.Assert;
 import org.junit.Test;
-import org.junit.jupiter.api.BeforeEach;
 
 import java.io.File;
+import java.util.ArrayList;
 
 public class Tests {
 
     private final ImportFile importFile = new ImportFile();
+    private ImageManager images = new ImageManager();
+    private File file = new File(System.getProperty("user.dir")+"Test assets/"+"test.xml");
 
     @Test
     public void findFileExtensionTest(){
@@ -20,6 +25,26 @@ public class Tests {
     public void locationSearchTest(){
         ImageManager imageManager = new ImageManager();
         Assert.assertNotNull(imageManager.searchImages("testLocation"));
+    }
+
+    @Test
+    public void imageManagerPictureTest(){
+        ArrayList<Picture> pictures = new ArrayList<>(images.getImages());
+        images.addImage("Arizona Clouds", null, "Arizona","Pretty clouds", ".jpg" );
+        for (Picture picture : pictures){
+            Assert.assertEquals("Arizona Clouds", picture.getTitle());
+            Assert.assertSame(null, picture.getImageLink());
+            Assert.assertEquals("Arizona", picture.getLocation());
+            Assert.assertEquals("Pretty clouds", picture.getDescription());
+            Assert.assertEquals(".jpg", picture.getFileExtension());
+        }
+    }
+
+    @Test
+    public void xmlReadWriteTest(){
+        PictureDataParser parser = new PictureDataParser();
+        parser.parsePictureData(file);
+
     }
 
 }
