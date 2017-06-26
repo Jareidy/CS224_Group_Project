4ebe60930a8305e03664d335bac1e7a6eb75f8fc
Jordan Reidy <jajreidy@gmail.com>
2017-06-26 13:36:20 -0400

    Broke up XMLHandler to smaller functions. Add functionality to parser and XMLHAndler. Correct incorrect pathnames.

--- test/Tests.java
+++ test/Tests.java
@@ -1,9 +1,7 @@
 import javafx.scene.image.Image;
-import model.ImageManager;
-import model.ImportFile;
-import model.Picture;
-import model.PictureDataParser;
+import model.*;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.io.File;
@@ -13,7 +11,20 @@ public class Tests {
 
     private final ImportFile importFile = new ImportFile();
     private ImageManager images = new ImageManager();
-    private File file = new File(System.getProperty("user.dir")+"Test assets/"+"test.xml");
+    private File file = new File(System.getProperty("user.dir")+"/Test assets/"+"test.xml");
+    ArrayList<Picture> pictures = new ArrayList<>(images.getImages());
+
+    @Before
+    public void createTestImageManager(){
+        PictureBuilder builder = new PictureBuilder();
+        builder.setTitle("Arizona Clouds");
+        builder.setImageLink(null);
+        builder.setExtension(".jpg");
+        builder.setDescription("Pretty clouds");
+        builder.setLocation("Arizona");
+        Picture newPicture = builder.build();
+        pictures.add(newPicture);
+    }
 
     @Test
     public void findFileExtensionTest(){
@@ -29,8 +40,6 @@ public class Tests {
 
     @Test
     public void imageManagerPictureTest(){
-        ArrayList<Picture> pictures = new ArrayList<>(images.getImages());
-        images.addImage("Arizona Clouds", null, "Arizona","Pretty clouds", ".jpg" );
         for (Picture picture : pictures){
             Assert.assertEquals("Arizona Clouds", picture.getTitle());
             Assert.assertSame(null, picture.getImageLink());
@@ -43,8 +52,11 @@ public class Tests {
     @Test
     public void xmlReadWriteTest(){
         PictureDataParser parser = new PictureDataParser();
+        ImageManager manager = new ImageManager();
+        manager.addImage(pictures.get(0));
+        XMLHandler.formatXmlFile(System.getProperty("user.dir")+"/Test assets/"+"Test.xml");
         parser.parsePictureData(file);
-
+        Assert.assertNotNull(parser.getImages());
     }
 
 }
