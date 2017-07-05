12f9bae187ee70b98f7154dfb72c44c51a25e971
Darcy Steele <dtsteele@bsu.edu>
2017-07-05 11:21:50 -0400

    Separated UserXMLHandler into smaller functions.

--- src/model/user/UsersXMLHandler.java
+++ src/model/user/UsersXMLHandler.java
@@ -9,29 +9,41 @@ import java.util.ArrayList;
 public class UsersXMLHandler {
 
     XMLBase xmlCreator = new XMLBase();
+    Document doc;
+    Element users;
 
     public void formatXmlFile(String localFileName){
-
         ArrayList<User> parsedUsers = UserManager.returnUsers();
-
-        Document doc = xmlCreator.docCreator();
-
+        doc = xmlCreator.docCreator();
         Element rootElement = doc.createElement("users");
         doc.appendChild(rootElement);
         for (User parsedUser: parsedUsers) {
-            Element users = doc.createElement("username");
-            rootElement.appendChild(users);
-            users.setAttribute("username", parsedUser.getUsername());
-
-            Element password = doc.createElement("password");
-            password.appendChild((doc.createTextNode(parsedUser.getPassword())));
-            users.appendChild(password);
-
-            Element emailAddress = doc.createElement("emailAddress");
-            emailAddress.appendChild((doc.createTextNode(parsedUser.getEmailAddress())));
-            users.appendChild(emailAddress);
+            createUserElement(rootElement,parsedUser);
+            formatPassword(users,parsedUser);
+            formatEmailAddress(users,parsedUser);
         }
-        xmlCreator.createXmlFile(doc, localFileName);
+        createFile(localFileName);
+    }
 
+    private void createUserElement(Element rootElement,User parsedUser) {
+        users = doc.createElement("username");
+        rootElement.appendChild(users);
+        users.setAttribute("username", parsedUser.getUsername());
+    }
+
+    private void formatPassword(Element users, User parsedUser) {
+        Element password = doc.createElement("password");
+        password.appendChild((doc.createTextNode(parsedUser.getPassword())));
+        users.appendChild(password);
+    }
+
+    private void formatEmailAddress(Element users,User parsedUser) {
+        Element emailAddress = doc.createElement("emailAddress");
+        emailAddress.appendChild((doc.createTextNode(parsedUser.getEmailAddress())));
+        users.appendChild(emailAddress);
+    }
+
+    public void createFile(String localFileName){
+        xmlCreator.createXmlFile(doc, localFileName);
     }
 }
